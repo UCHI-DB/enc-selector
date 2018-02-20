89978a656cf3093292e4b0f520f2d693bb9f135c
Harper Jiang <harperjiang@msn.com>
2018-02-19 18:33:14 -0600

    Enable Compression on Encoding

--- src/main/java/edu/uchicago/cs/encsel/parquet/ParquetWriterBuilder.java
+++ src/main/java/edu/uchicago/cs/encsel/parquet/ParquetWriterBuilder.java
@@ -93,4 +93,13 @@ public class ParquetWriterBuilder extends Builder<List<String>, ParquetWriterBui
                 .withPageSize(ParquetWriter.DEFAULT_PAGE_SIZE)
                 .withDictionaryPageSize(SCALE * ParquetWriter.DEFAULT_PAGE_SIZE).build();
     }
+
+    public static ParquetWriter<List<String>> buildCompressed(Path file, MessageType schema, CompressionCodecName codec) throws IOException {
+        ParquetWriterBuilder builder = new ParquetWriterBuilder(file, schema, new EncValuesWriterFactory());
+
+        return builder.withValidation(false).withCompressionCodec(codec)
+                .withRowGroupSize(ParquetWriter.DEFAULT_BLOCK_SIZE)
+                .withPageSize(ParquetWriter.DEFAULT_PAGE_SIZE)
+                .withDictionaryPageSize(SCALE * ParquetWriter.DEFAULT_PAGE_SIZE).build();
+    }
 }
