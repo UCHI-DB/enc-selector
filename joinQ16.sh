6d652ff0f479a238787f888c40fceddb147b76b6
Tranway1 <tranway@qq.com>
2018-02-25 05:06:40 +0000

    scale to 5 machine

--- joinQ16.sh
+++ joinQ16.sh
@@ -2,13 +2,18 @@
 # Chunwei Liu
 # RLE BIT_PACKED 
 BASEDIR=$(pwd)
-for p_key in BP RLE PLAIN DICT DELTABP; do
-	for l_key in BP RLE PLAIN DICT DELTABP; do
-		echo "*********p_key: ${p_key} l_key: ${l_key}********"
-		cd ~/enc-selector/
-		java -cp target/enc-selector-0.0.1-SNAPSHOT.jar:target/enc-selector-0.0.1-SNAPSHOT-jar-with-dependencies.jar edu.uchicago.cs.encsel.query.JoinFileProducer $p_key $l_key 
-		java -cp target/enc-selector-0.0.1-SNAPSHOT.jar:target/enc-selector-0.0.1-SNAPSHOT-jar-with-dependencies.jar edu.uchicago.cs.encsel.query.HashJoinTool
-		#cd $BASEDIR
+for i in 1 5 10 15; do
+	cd ~/tpch-generator/dbgen/
+        ./dbgen -f -s $i
+        echo "scale:$i"
+	for p_key in BP RLE PLAIN DICT DELTABP; do
+		for l_key in BP RLE PLAIN DICT DELTABP; do
+			echo "${p_key},${l_key} part.0 lineitem.1"
+			cd ~/enc-selector/
+			java -cp target/enc-selector-0.0.1-SNAPSHOT.jar:target/enc-selector-0.0.1-SNAPSHOT-jar-with-dependencies.jar edu.uchicago.cs.encsel.query.JoinFileProducer $p_key $l_key 
+			java -cp target/enc-selector-0.0.1-SNAPSHOT.jar:target/enc-selector-0.0.1-SNAPSHOT-jar-with-dependencies.jar edu.uchicago.cs.encsel.query.HashJoinTool
+			#cd $BASEDIR
+		done
 	done
 done 
 
--- q6.sh
+++ q6.sh
@@ -1,15 +1,46 @@
 #! /bin/sh
 # Chunwei Liu
 BASEDIR=$(pwd)
-for i in 1 10 30 50 70 90
-do
-	echo "$i"
+for i in 1 5 10 15; do
 	cd ~/tpch-generator/dbgen/
-	./dbgen -f -s $i
-	cd ~/enc-selector/
-	java -cp target/enc-selector-0.0.1-SNAPSHOT-jar-with-dependencies.jar edu.uchicago.cs.encsel.query.tpch.LoadTPCH4Plain 
-	mvn surefire:test -Dtest=TPCHQueryTest
-	java -cp target/enc-selector-0.0.1-SNAPSHOT-jar-with-dependencies.jar edu.uchicago.cs.encsel.query.tpch.LoadTPCH4Default
-	mvn surefire:test -Dtest=TPCHQueryTest
-	cd $BASEDIR
+        ./dbgen -f -s $i
+	echo "scale:$i"
+	for col in 4; do
+		for enc in BIT_PACKED PLAIN_DICTIONARY PLAIN RLE DELTA_BINARY_PACKED; do
+			echo "$col $enc"
+			cd ~/enc-selector/
+			java -cp target/enc-selector-0.0.1-SNAPSHOT.jar:target/enc-selector-0.0.1-SNAPSHOT-jar-with-dependencies.jar edu.uchicago.cs.encsel.query.ScanFileProducer $col $enc
+			java -cp target/enc-selector-0.0.1-SNAPSHOT.jar:target/enc-selector-0.0.1-SNAPSHOT-jar-with-dependencies.jar edu.uchicago.cs.encsel.query.Q6ScanTool
+		done
+	done
+
+	for col in 5; do
+                for enc in PLAIN_DICTIONARY PLAIN; do
+                        echo "$col $enc"
+                        cd ~/enc-selector/
+                        java -cp target/enc-selector-0.0.1-SNAPSHOT.jar:target/enc-selector-0.0.1-SNAPSHOT-jar-with-dependencies.jar edu.uchicago.cs.encsel.query.ScanFileProducer $col $enc
+                        java -cp target/enc-selector-0.0.1-SNAPSHOT.jar:target/enc-selector-0.0.1-SNAPSHOT-jar-with-dependencies.jar edu.uchicago.cs.encsel.query.Q6ScanTool
+                done
+        done
+	
+
+	for col in 6; do
+                for enc in PLAIN_DICTIONARY PLAIN; do
+                        echo "$col $enc"
+                        cd ~/enc-selector/
+                        java -cp target/enc-selector-0.0.1-SNAPSHOT.jar:target/enc-selector-0.0.1-SNAPSHOT-jar-with-dependencies.jar edu.uchicago.cs.encsel.query.ScanFileProducer $col $enc
+                        java -cp target/enc-selector-0.0.1-SNAPSHOT.jar:target/enc-selector-0.0.1-SNAPSHOT-jar-with-dependencies.jar edu.uchicago.cs.encsel.query.Q6ScanTool
+                done
+        done
+
+	for col in 10; do
+                for enc in DELTA_LENGTH_BYTE_ARRAY DELTA_BYTE_ARRAY PLAIN_DICTIONARY PLAIN; do
+                        echo "$col $enc"
+                        cd ~/enc-selector/
+                        java -cp target/enc-selector-0.0.1-SNAPSHOT.jar:target/enc-selector-0.0.1-SNAPSHOT-jar-with-dependencies.jar edu.uchicago.cs.encsel.query.ScanFileProducer $col $enc
+                        java -cp target/enc-selector-0.0.1-SNAPSHOT.jar:target/enc-selector-0.0.1-SNAPSHOT-jar-with-dependencies.jar edu.uchicago.cs.encsel.query.Q6ScanTool
+                done
+        done
+
+cd $BASEDIR
 done 
