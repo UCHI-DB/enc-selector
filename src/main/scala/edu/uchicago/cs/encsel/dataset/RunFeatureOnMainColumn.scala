4525078bea1f97ae95f7d207311ac87cdde3bcd7
Hao Jiang <harperjiang@msn.com>
2018-02-25 12:42:08 -0600

    Scan only on snappy

--- src/main/scala/edu/uchicago/cs/encsel/dataset/RunFeatureOnMainColumn.scala
+++ src/main/scala/edu/uchicago/cs/encsel/dataset/RunFeatureOnMainColumn.scala
@@ -23,7 +23,7 @@
 package edu.uchicago.cs.encsel.dataset
 
 import edu.uchicago.cs.encsel.dataset.feature._
-import edu.uchicago.cs.encsel.dataset.feature.report.{ScanCompressedTimeUsage, ScanCompressedTimeUsageNoSnappy}
+import edu.uchicago.cs.encsel.dataset.feature.report.{ScanCompressedTimeUsage, ScanCompressedTimeUsageNoSnappy, ScanCompressedTimeUsageSnappy}
 import edu.uchicago.cs.encsel.dataset.persist.Persistence
 import edu.uchicago.cs.encsel.dataset.persist.jpa.{ColumnWrapper, JPAPersistence}
 import org.slf4j.LoggerFactory
@@ -40,7 +40,7 @@ object RunFeatureOnMainColumn extends App {
   val persist = new JPAPersistence
 
   // val missed = Seq(new MiscEncFileSize(new BitVectorEncoding))
-  val missed = Seq(ScanCompressedTimeUsageNoSnappy)
+  val missed = Seq(ScanCompressedTimeUsageSnappy)
 
   val prefix = args.length match {
     case gt if gt > 0 => args(0)
