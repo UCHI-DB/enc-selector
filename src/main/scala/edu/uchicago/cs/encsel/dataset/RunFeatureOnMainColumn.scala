196a78b2207d378ff4e17f624b23f526e6080c51
Hao Jiang <harperjiang@msn.com>
2018-03-05 00:19:39 -0600

    Rescan all columns

--- src/main/scala/edu/uchicago/cs/encsel/dataset/RunFeatureOnMainColumn.scala
+++ src/main/scala/edu/uchicago/cs/encsel/dataset/RunFeatureOnMainColumn.scala
@@ -23,7 +23,7 @@
 package edu.uchicago.cs.encsel.dataset
 
 import edu.uchicago.cs.encsel.dataset.feature._
-import edu.uchicago.cs.encsel.dataset.feature.report.{ScanCompressedTimeUsage, ScanCompressedTimeUsageNoSnappy, ScanCompressedTimeUsageSnappy, SelTimeUsage}
+import edu.uchicago.cs.encsel.dataset.feature.report._
 import edu.uchicago.cs.encsel.dataset.persist.Persistence
 import edu.uchicago.cs.encsel.dataset.persist.jpa.{ColumnWrapper, JPAPersistence}
 import org.slf4j.LoggerFactory
@@ -40,7 +40,7 @@ object RunFeatureOnMainColumn extends App {
   val persist = new JPAPersistence
 
   // val missed = Seq(new MiscEncFileSize(new BitVectorEncoding))
-  val missed = Seq(SelTimeUsage)
+  val missed = Seq(ScanTimeUsage)
 
   val prefix = args.length match {
     case gt if gt > 0 => args(0)
