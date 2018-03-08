619d04b92fb8f84c042256764abeba6576467a15
Hao Jiang <harperjiang@msn.com>
2018-03-08 07:53:13 -0600

    subattr.

--- src/main/scala/edu/uchicago/cs/encsel/dataset/RunFeatureOnMainColumn.scala
+++ src/main/scala/edu/uchicago/cs/encsel/dataset/RunFeatureOnMainColumn.scala
@@ -25,6 +25,7 @@ package edu.uchicago.cs.encsel.dataset
 import edu.uchicago.cs.encsel.dataset.feature._
 import edu.uchicago.cs.encsel.dataset.feature.report._
 import edu.uchicago.cs.encsel.dataset.feature.resource.ScanTimeUsage
+import edu.uchicago.cs.encsel.dataset.feature.subattr.SubattrScanTimeUsage
 import edu.uchicago.cs.encsel.dataset.persist.Persistence
 import edu.uchicago.cs.encsel.dataset.persist.jpa.{ColumnWrapper, JPAPersistence}
 import org.slf4j.LoggerFactory
@@ -41,7 +42,7 @@ object RunFeatureOnMainColumn extends App {
   val persist = new JPAPersistence
 
   // val missed = Seq(new MiscEncFileSize(new BitVectorEncoding))
-  val missed = Seq(ScanTimeUsage)
+  val missed = Seq(SubattrScanTimeUsage)
 
   val prefix = args.length match {
     case gt if gt > 0 => args(0)
