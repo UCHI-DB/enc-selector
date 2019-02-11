969de34ae77649b41472bb5bf07979a1cb72e5d8
Harper <harperjiang@msn.com>
2019-02-11 16:13:24 -0600

    Fix test case

--- src/main/java/edu/uchicago/cs/encsel/query/MemBufferPrimitiveConverter.java
+++ src/main/java/edu/uchicago/cs/encsel/query/MemBufferPrimitiveConverter.java
@@ -3,7 +3,6 @@ package edu.uchicago.cs.encsel.query;
 import org.apache.parquet.column.Dictionary;
 import org.apache.parquet.io.api.Binary;
 import org.apache.parquet.io.api.PrimitiveConverter;
-import org.apache.parquet.schema.PrimitiveType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +11,14 @@ public class MemBufferPrimitiveConverter extends PrimitiveConverter {
     private Dictionary dictionary;
 
     private List<Object> buffer = new ArrayList<>();
+
+    public List<Object> getBuffer() {
+        return buffer;
+    }
+
+    public void setBuffer(List<Object> buffer) {
+        this.buffer = buffer;
+    }
 
     public MemBufferPrimitiveConverter() {
     }
