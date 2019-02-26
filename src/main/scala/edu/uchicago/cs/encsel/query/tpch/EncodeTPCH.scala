c8d2b0020f4cb6b892cb76bce354388996ce2870
Harper <harperjiang@msn.com>
2019-02-26 17:09:16 -0600

    EncodeTPCH with Abadi and DDES

--- src/main/scala/edu/uchicago/cs/encsel/query/tpch/EncodeTPCH.scala
+++ src/main/scala/edu/uchicago/cs/encsel/query/tpch/EncodeTPCH.scala
@@ -35,9 +35,9 @@ object EncodeTPCH extends App {
 
   def plainGzip(folder: String, inputsuffix: String, outputsuffix: String): Unit = {
     TPCHSchema.schemas.foreach(schema => {
-      schema.getColumns.forEach(cd=>{
+      schema.getColumns.forEach(cd => {
         EncContext.encoding.get().put(cd.toString, Encoding.PLAIN)
-        EncContext.context.get().put(cd.toString, Array[Object]("0","0"))
+        EncContext.context.get().put(cd.toString, Array[Object]("0", "0"))
       })
       ParquetWriterHelper.write(
         new File("%s%s%s".format(folder, schema.getName, inputsuffix)).toURI,
@@ -51,7 +51,7 @@ object EncodeTPCH extends App {
 
   def dictGzip(folder: String, inputsuffix: String, outputsuffix: String): Unit = {
     TPCHSchema.schemas.foreach(schema => {
-      schema.getColumns.forEach(cd=>{
+      schema.getColumns.forEach(cd => {
         EncContext.encoding.get().put(cd.toString, Encoding.PLAIN_DICTIONARY)
       })
       ParquetWriterHelper.write(
@@ -79,9 +79,9 @@ object EncodeTPCH extends App {
   def plain(folder: String, inputsuffix: String, outputsuffix: String): Unit = {
     // Load TPCH
     TPCHSchema.schemas.foreach(schema => {
-      schema.getColumns.forEach(cd=>{
+      schema.getColumns.forEach(cd => {
         EncContext.encoding.get().put(cd.toString, Encoding.PLAIN)
-        EncContext.context.get().put(cd.toString, Array[Object]("0","0"))
+        EncContext.context.get().put(cd.toString, Array[Object]("0", "0"))
       })
       ParquetWriterHelper.write(
         new File("%s%s%s".format(folder, schema.getName, inputsuffix)).toURI,
@@ -94,7 +94,7 @@ object EncodeTPCH extends App {
 
   def dict(folder: String, inputsuffix: String, outputsuffix: String): Unit = {
     TPCHSchema.schemas.foreach(schema => {
-      schema.getColumns.forEach(cd=>{
+      schema.getColumns.forEach(cd => {
         EncContext.encoding.get().put(cd.toString, Encoding.PLAIN_DICTIONARY)
       })
       ParquetWriterHelper.write(
@@ -108,7 +108,7 @@ object EncodeTPCH extends App {
 
   def parquet(folder: String, inputsuffix: String, outputsuffix: String): Unit = {
     TPCHSchema.schemas.foreach(schema => {
-      schema.getColumns.forEach(cd=>{
+      schema.getColumns.forEach(cd => {
         EncContext.encoding.get().remove(cd.toString)
       })
       ParquetWriterHelper.write(
@@ -119,6 +119,88 @@ object EncodeTPCH extends App {
         false)
     })
   }
+
+  def abadi(folder: String, inputsuffix: String, outputsuffix: String): Unit = {
+    var lineitemSchema = TPCHSchema.lineitemSchema
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(0).toString, Encoding.RLE)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(1).toString, Encoding.PLAIN_DICTIONARY)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(2).toString, Encoding.PLAIN_DICTIONARY)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(3).toString, Encoding.PLAIN_DICTIONARY)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(4).toString, Encoding.PLAIN_DICTIONARY)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(5).toString, Encoding.PLAIN)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(6).toString, Encoding.PLAIN)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(7).toString, Encoding.PLAIN)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(8).toString, Encoding.PLAIN_DICTIONARY)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(9).toString, Encoding.PLAIN_DICTIONARY)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(10).toString, Encoding.PLAIN_DICTIONARY)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(11).toString, Encoding.PLAIN_DICTIONARY)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(12).toString, Encoding.PLAIN_DICTIONARY)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(13).toString, Encoding.PLAIN_DICTIONARY)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(14).toString, Encoding.PLAIN_DICTIONARY)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(15).toString, Encoding.PLAIN)
+    ParquetWriterHelper.write(
+      new File("%s%s%s".format(folder, lineitemSchema.getName, inputsuffix)).toURI,
+      lineitemSchema,
+      new File("%s%s%s".format(folder, lineitemSchema.getName, outputsuffix)).toURI,
+      "\\|",
+      false, CompressionCodecName.GZIP)
+  }
+
+  def ddes_store(folder: String, inputsuffix: String, outputsuffix: String): Unit = {
+    var lineitemSchema = TPCHSchema.lineitemSchema
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(0).toString, Encoding.DELTA_BINARY_PACKED)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(1).toString, Encoding.BIT_PACKED)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(2).toString, Encoding.BIT_PACKED)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(3).toString, Encoding.BIT_PACKED)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(4).toString, Encoding.PLAIN_DICTIONARY)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(5).toString, Encoding.PLAIN_DICTIONARY)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(6).toString, Encoding.PLAIN_DICTIONARY)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(7).toString, Encoding.PLAIN_DICTIONARY)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(8).toString, Encoding.PLAIN_DICTIONARY)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(9).toString, Encoding.PLAIN_DICTIONARY)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(10).toString, Encoding.PLAIN_DICTIONARY)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(11).toString, Encoding.PLAIN_DICTIONARY)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(12).toString, Encoding.PLAIN_DICTIONARY)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(13).toString, Encoding.PLAIN_DICTIONARY)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(14).toString, Encoding.PLAIN_DICTIONARY)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(15).toString, Encoding.DELTA_LENGTH_BYTE_ARRAY)
+    ParquetWriterHelper.write(
+      new File("%s%s%s".format(folder, lineitemSchema.getName, inputsuffix)).toURI,
+      lineitemSchema,
+      new File("%s%s%s".format(folder, lineitemSchema.getName, outputsuffix)).toURI,
+      "\\|",
+      false, CompressionCodecName.GZIP)
+  }
+
+
+  def ddes_speed(folder: String, inputsuffix: String, outputsuffix: String): Unit = {
+    var lineitemSchema = TPCHSchema.lineitemSchema
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(0).toString, Encoding.RLE)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(1).toString, Encoding.BIT_PACKED)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(1).toString, Encoding.BIT_PACKED)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(1).toString, Encoding.BIT_PACKED)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(2).toString, Encoding.RLE)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(3).toString, Encoding.RLE)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(4).toString, Encoding.RLE)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(5).toString, Encoding.PLAIN)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(6).toString, Encoding.PLAIN_DICTIONARY)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(7).toString, Encoding.PLAIN_DICTIONARY)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(8).toString, Encoding.PLAIN)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(9).toString, Encoding.PLAIN)
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(10).toString, Encoding.PLAIN) // GZIP
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(11).toString, Encoding.PLAIN) // GZIP
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(12).toString, Encoding.PLAIN) // GZIP
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(13).toString, Encoding.DELTA_LENGTH_BYTE_ARRAY) // GZIP
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(14).toString, Encoding.DELTA_LENGTH_BYTE_ARRAY) // GZIP
+    EncContext.encoding.get().put(lineitemSchema.getColumns().get(15).toString, Encoding.DELTA_LENGTH_BYTE_ARRAY) // GZIP
+    ParquetWriterHelper.write(
+      new File("%s%s%s".format(folder, lineitemSchema.getName, inputsuffix)).toURI,
+      lineitemSchema,
+      new File("%s%s%s".format(folder, lineitemSchema.getName, outputsuffix)).toURI,
+      "\\|",
+      false, CompressionCodecName.GZIP)
+  }
+
 
   val folder = args(0)
   val inputsuffix = ".tbl"
