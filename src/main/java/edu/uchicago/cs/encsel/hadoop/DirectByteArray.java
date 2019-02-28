a36830a9fe88c9b084bb363a0760ac7b5fb1d5d0
harper <harperjiang@msn.com>
2019-02-28 03:33:22 -0600

    Create MemoryFileSystem

---
+++ src/main/java/edu/uchicago/cs/encsel/hadoop/DirectByteArray.java
@@ -0,0 +1,69 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ * Contributors:
+ *     Hao Jiang - initial API and implementation
+ */
+
+package edu.uchicago.cs.encsel.hadoop;
+
+import jdk.nashorn.internal.runtime.arrays.ArrayIndex;
+import sun.misc.Unsafe;
+
+class DirectByteArray {
+
+    Unsafe unsafe = Unsafe.getUnsafe();
+
+    private final long startIndex;
+
+    private long size;
+
+    public DirectByteArray(long size) {
+        this.size = size;
+        startIndex = unsafe.allocateMemory(size);
+        unsafe.setMemory(startIndex, size, (byte) 0);
+    }
+
+    public void set(long index, byte value) {
+        if (index >= size)
+            throw new ArrayIndexOutOfBoundsException();
+        unsafe.putByte(index, value);
+    }
+
+    public int get(long index) {
+        if (index >= size)
+            throw new ArrayIndexOutOfBoundsException();
+        return unsafe.getByte(index);
+    }
+
+    public void destroy() {
+        unsafe.freeMemory(startIndex);
+    }
+
+    public int copy(long index, byte[] buffer, int offset, int length) {
+        int copylen = length;
+        if (index + length >= size)
+            copylen = (int) (size - 1 - index);
+        unsafe.copyMemory(null, startIndex + index, buffer, Unsafe.ARRAY_BYTE_BASE_OFFSET + offset, copylen);
+        return copylen;
+    }
+
+    public long size() {
+        return size;
+    }
+}
