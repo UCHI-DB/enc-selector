9c576f45087d2acaabc670ac7af34e1f93d6d261
harper <harperjiang@msn.com>
2019-02-27 22:14:06 -0600

    Fix bug in encode

--- src/main/scala/edu/uchicago/cs/encsel/query/tpch/EncodeTPCH.scala
+++ src/main/scala/edu/uchicago/cs/encsel/query/tpch/EncodeTPCH.scala
@@ -139,6 +139,14 @@ object EncodeTPCH extends App {
     EncContext.encoding.get().put(lineitemSchema.getColumns().get(13).toString, Encoding.PLAIN_DICTIONARY)
     EncContext.encoding.get().put(lineitemSchema.getColumns().get(14).toString, Encoding.PLAIN_DICTIONARY)
     EncContext.encoding.get().put(lineitemSchema.getColumns().get(15).toString, Encoding.PLAIN)
+
+
+    EncContext.context.get().put(lineitemSchema.getColumns().get(0).toString, Array("28", "180000000"))
+    EncContext.context.get().put(lineitemSchema.getColumns().get(1).toString, Array("21", "2000000"))
+    EncContext.context.get().put(lineitemSchema.getColumns().get(2).toString, Array("20", "300000"))
+    EncContext.context.get().put(lineitemSchema.getColumns().get(3).toString, Array("3", "7"))
+    EncContext.context.get().put(lineitemSchema.getColumns().get(4).toString, Array("7", "100"))
+
     ParquetWriterHelper.write(
       new File("%s%s%s".format(folder, lineitemSchema.getName, inputsuffix)).toURI,
       lineitemSchema,
