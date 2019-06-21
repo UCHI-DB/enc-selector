569d9c386f4787032f9e926049ff12e8610ab27c
harper <harperjiang@msn.com>
2019-06-21 03:07:12 -0500

    Compress String Columns

--- src/main/scala/edu/uchicago/cs/encsel/ptnmining/EncodeSubColumn.scala
+++ src/main/scala/edu/uchicago/cs/encsel/ptnmining/EncodeSubColumn.scala
@@ -33,11 +33,11 @@ import scala.collection.JavaConverters._
 object EncodeAllSubColumn extends App {
 
   val persist = new JPAPersistence
-  val columns = persist.em.createQuery("SELECT c FROM Column c WHERE c.parentWrapper IS NOT NULL AND c.dataType = :dt and c.id >= 63451",
-    classOf[ColumnWrapper]).setParameter("dt", DataType.INTEGER).getResultList.asScala
+  val columns = persist.em.createQuery("SELECT c FROM Column c WHERE c.parentWrapper IS NOT NULL AND c.dataType = :dt",
+    classOf[ColumnWrapper]).setParameter("dt", DataType.STRING).getResultList.asScala
   columns.foreach(colw => {
     println(colw.id)
-    colw.replaceFeatures(ParquetEncFileSize.extract(colw))
+//    colw.replaceFeatures(ParquetEncFileSize.extract(colw))
     colw.replaceFeatures(ParquetCompressFileSize.extract(colw))
     persist.save(Seq(colw))
   })
