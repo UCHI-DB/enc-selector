40afc4ebd833f545a15c9a58f78c6d6e4bdab142
harper <harperjiang@msn.com>
2019-10-09 11:24:00 -0500

    Run ORC writer

--- src/main/scala/edu/uchicago/cs/encsel/dataset/RunFeature.scala
+++ src/main/scala/edu/uchicago/cs/encsel/dataset/RunFeature.scala
@@ -22,63 +22,79 @@
 
 package edu.uchicago.cs.encsel.dataset
 
+import java.util.concurrent.atomic.AtomicInteger
+
+import edu.uchicago.cs.encsel.dataset.column.Column
 import edu.uchicago.cs.encsel.dataset.feature._
-import edu.uchicago.cs.encsel.dataset.feature.encode.parquet.resource.ScanTimeUsage
+import edu.uchicago.cs.encsel.dataset.feature.encode.orc.OrcEncFileSize
 import edu.uchicago.cs.encsel.dataset.persist.Persistence
 import edu.uchicago.cs.encsel.dataset.persist.jpa.JPAPersistence
+import edu.uchicago.cs.encsel.model.DataType
 import org.slf4j.LoggerFactory
 
 /**
   * Created by harper on 4/23/17.
   */
-object RunFeature extends App {
+class FeatureRunner {
 
-  val logger = LoggerFactory.getLogger(getClass)
+  var missed = Seq(OrcEncFileSize)
 
-  val persist = new JPAPersistence
+  def run(args: Array[String]): Unit = {
+    val logger = LoggerFactory.getLogger(getClass)
 
-  // val missed = Seq(new MiscEncFileSize(new BitVectorEncoding))
-  val missed = Seq(ScanTimeUsage)
+    val persist = new JPAPersistence
 
-  val prefix = args.length match {
-    case gt if gt > 0 => args(0)
-    case _ => ""
-  }
-
-  val filter = args.length match {
-    case gt if gt > 0 =>
-      args(1) match {
-        case "none" => null
-        case "firstn" => Filter.firstNFilter(args(2).toInt)
-        case "iid" => Filter.iidSamplingFilter(args(2).toDouble)
-        case "size" => Filter.sizeFilter(args(2).toInt)
-        case "minsize" => Filter.minSizeFilter(args(2).toInt, args(3).toDouble)
-        case _ => throw new IllegalArgumentException(args(1))
-      }
-    case _ => null
-  }
-
-  Features.extractors.clear()
-  Features.extractors ++= missed
-
-  val persistence = Persistence.get
-  val columns = persistence.load().toList
-  val size = columns.size
-  var counter = 0
-  columns.foreach(column => {
-    counter += 1
-    System.out.println("Processing %d / %d : %s".format(counter, size, column.colFile))
-    try {
-      if (filter == null) {
-        column.replaceFeatures(Features.extract(column))
-      } else {
-        column.replaceFeatures(Features.extract(column, filter, prefix))
-      }
-      persistence.save(Seq(column))
-    } catch {
-      case e: Exception => {
-        logger.warn("Failed during processing", e)
-      }
+    val prefix = args.length match {
+      case gt if gt > 0 => args(0)
+      case _ => ""
     }
-  })
+
+    val filter = args.length match {
+      case gt if gt > 0 =>
+        args(1) match {
+          case "none" => null
+          case "firstn" => Filter.firstNFilter(args(2).toInt)
+          case "iid" => Filter.iidSamplingFilter(args(2).toDouble)
+          case "size" => Filter.sizeFilter(args(2).toInt)
+          case "minsize" => Filter.minSizeFilter(args(2).toInt, args(3).toDouble)
+          case _ => throw new IllegalArgumentException(args(1))
+        }
+      case _ => null
+    }
+
+    Features.extractors.clear()
+    Features.extractors ++= missed
+
+    val persistence = Persistence.get
+    val columns = getColumns(persistence)
+
+    val size = columns.size
+    var counter = new AtomicInteger(0)
+
+    columns.toList.par.foreach(column => {
+      val current = counter.addAndGet(1)
+      System.out.println("Processing %d / %d : %s".format(current, size, column.colFile))
+      try {
+        if (filter == null) {
+          column.replaceFeatures(Features.extract(column))
+        } else {
+          column.replaceFeatures(Features.extract(column, filter, prefix))
+        }
+        persistence.save(Seq(column))
+      } catch {
+        case e: Exception => {
+          logger.warn("Failed during processing", e)
+        }
+      }
+    })
+  }
+
+  def getColumns(persistence: Persistence): List[Column] = {
+    val columns = persistence.load().filter(col => col.dataType == DataType.INTEGER || col.dataType == DataType.STRING).toList
+    columns
+  }
+}
+
+object RunFeature extends App {
+  new FeatureRunner().run(args)
 }
--- src/main/scala/edu/uchicago/cs/encsel/dataset/RunFeatureOnMainColumn.scala
+++ src/main/scala/edu/uchicago/cs/encsel/dataset/RunFeatureOnMainColumn.scala
@@ -22,12 +22,9 @@
 
 package edu.uchicago.cs.encsel.dataset
 
-import edu.uchicago.cs.encsel.dataset.feature._
-import edu.uchicago.cs.encsel.dataset.feature.encode.parquet.{ParquetCompressTimeUsage, ScanCompressedTimeUsage}
-import edu.uchicago.cs.encsel.dataset.feature.encode.parquet.resource.{EncTimeUsage, ScanTimeUsage}
+import edu.uchicago.cs.encsel.dataset.column.Column
 import edu.uchicago.cs.encsel.dataset.persist.Persistence
 import edu.uchicago.cs.encsel.dataset.persist.jpa.{ColumnWrapper, JPAPersistence}
-import org.slf4j.LoggerFactory
 
 import scala.collection.JavaConverters._
 
@@ -36,55 +33,15 @@ import scala.collection.JavaConverters._
   */
 object RunFeatureOnMainColumn extends App {
 
-  val logger = LoggerFactory.getLogger(getClass)
-
-  val persist = new JPAPersistence
-
-  // val missed = Seq(new MiscEncFileSize(new BitVectorEncoding))
-  //  val missed = Seq(new SimilarWords)
-
-  val prefix = args.length match {
-    case gt if gt > 0 => args(0)
-    case _ => ""
-  }
-
-  val filter = args.length match {
-    case gt if gt > 0 =>
-      args(1) match {
-        case "none" => null
-        case "firstn" => Filter.firstNFilter(args(2).toInt)
-        case "iid" => Filter.iidSamplingFilter(args(2).toDouble)
-        case "size" => Filter.sizeFilter(args(2).toInt)
-        case "minsize" => Filter.minSizeFilter(args(2).toInt, args(3).toDouble)
-        case _ => throw new IllegalArgumentException(args(1))
-      }
-    case _ => null
-  }
-
-  Features.extractors.clear()
-  Features.extractors ++= Seq(EncTimeUsage, ParquetCompressTimeUsage, ScanTimeUsage, ScanCompressedTimeUsage)
-
-  val persistence = Persistence.get
-  val columns = persist.ems.get.createNativeQuery("SELECT \n    cd.*\nFROM\n    col_data cd\n        JOIN\n    " +
-    "feature size ON size.col_id = cd.id\n        AND size.type = 'EncFileSize'\n        " +
-    "AND size.name = 'PLAIN_file_size'\n        AND size.value > 1000000\nWHERE\n    cd.parent_id IS NULL",
-    classOf[ColumnWrapper]).getResultList.asScala.toList.map(_.asInstanceOf[ColumnWrapper])
-  val size = columns.size
-  var counter = 0
-  columns.foreach(column => {
-    counter += 1
-    System.out.println("Processing %d, %d / %d : %s".format(column.id, counter, size, column.colFile))
-    try {
-      if (filter == null) {
-        column.replaceFeatures(Features.extract(column))
-      } else {
-        column.replaceFeatures(Features.extract(column, filter, prefix))
-      }
-      persistence.save(Seq(column))
-    } catch {
-      case e: Exception => {
-        logger.warn("Failed during processing", e)
-      }
+  val featureRunner = new FeatureRunner {
+    override def getColumns(persistence: Persistence) = {
+      val data = persistence.asInstanceOf[JPAPersistence].ems.get.createNativeQuery("SELECT \n    cd.*\nFROM\n    col_data cd\n        JOIN\n    " +
+        "feature size ON size.col_id = cd.id\n        AND size.type = 'EncFileSize'\n        " +
+        "AND size.name = 'PLAIN_file_size'\n        AND size.value > 1000000\nWHERE\n    cd.parent_id IS NULL",
+        classOf[ColumnWrapper]).getResultList
+      data.asScala.map(_.asInstanceOf[Column]).toList
     }
-  })
+  }
+
+  featureRunner.run(args)
 }
