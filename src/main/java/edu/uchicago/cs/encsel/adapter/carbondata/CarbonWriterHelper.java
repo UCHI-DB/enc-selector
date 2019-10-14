6c70812faae5e4454df4cba37a6d46512f04385f
Harper <harperjiang@msn.com>
2019-10-14 15:05:42 -0500

    CarbonWriterHelper skeleton

--- src/main/java/edu/uchicago/cs/encsel/adapter/carbondata/CarbonWriterHelper.java
+++ src/main/java/edu/uchicago/cs/encsel/adapter/carbondata/CarbonWriterHelper.java
@@ -32,91 +32,91 @@ import org.apache.carbondata.common.exceptions.sql.InvalidLoadOptionException;
 import org.apache.carbondata.core.metadata.datatype.DataType;
 import org.apache.commons.io.FileUtils;
 //
-//import org.apache.carbondata.sdk.file.CarbonWriter;
-//import org.apache.carbondata.sdk.file.Field;
-//import org.apache.carbondata.sdk.file.Schema;
+import org.apache.carbondata.sdk.file.CarbonWriter;
+import org.apache.carbondata.sdk.file.Field;
+import org.apache.carbondata.sdk.file.Schema;
 
 public class CarbonWriterHelper {
 
-//    public static void CSVParseAndWrite(String inputPath, String outputDir, Field[] originalFields, String regex){
-//        Field[] fields = originalFields;
-//        System.out.println("Input file schema: ");
-//        for(Field f:fields){
-//            System.out.print("\t" + f.getFieldName() +" " +f.getDataType().toString());
-//        }
-//        System.out.print("\n");
-//        if (!ProcessingContext.context.isEmpty()){
-//            System.out.println("Sub-column schema transformation: ");
-//            for (int i=fields.length-1; i>=0; i--){
-//                if (ProcessingContext.context.containsKey(fields[i].getFieldName())){
-//                    String[] curMeta = ProcessingContext.context.get(fields[i].getFieldName());
-//                    SubAttConvertor curConv = new SubAttConvertor(fields[i].getFieldName(), curMeta[0],curMeta[2],curMeta[1], fields[i].getDataType(),(curMeta.length==4?curMeta[3]:null));
-//                    ProcessingContext.merge.put(i,curConv);
-//                    Field[] subFields = new Field[curConv.getNumSubCols()];
-//
-//                    for(int j=0;j<curConv.getNumSubCols();j++){
-//                        subFields[j] = new Field(curConv.getSubcol()[j],curConv.getSubTypes()[j]);
-//                    }
-//                    fields = mergeArr(fields,i,subFields);
-//                }
-//            }
-//            System.out.println("Translated file schema: ");
-//            for(Field f:fields){
-//                System.out.print("\t" + f.getFieldName() +" " +f.getDataType().toString());
-//            }
-//
-//        }
-//
-//        try {
-//            FileUtils.deleteDirectory(new File(outputDir));
-//            BufferedReader reader = new BufferedReader(new FileReader(new File(inputPath)));
-//            Map<String, String> map = new HashMap<String, String>();
-//            map.put("complex_delimiter_level_1", "#");
-//            CarbonWriter writer = CarbonWriter.builder()
-//                    .outputPath(outputDir)
-//                    .withLoadOptions(map)
-//                    .buildWriterForCSVInput(new Schema(fields));
-//            String line;
-//            boolean skipHeader = true;
-//            // Skip header line
-//            line = skipHeader ? reader.readLine() : null;
-//
-//            if (ProcessingContext.merge.isEmpty()){
-//                while ((line = reader.readLine()) != null) {
-//                    // Handle the empty entry for each line
-//                    String[] dataArray = parseByRegex(line,regex,fields.length);
-//                    writer.write(dataArray);
-//                }
-//
-//                reader.close();
-//                writer.close();
-//            }
-//            else{
-//                while ((line = reader.readLine()) != null) {
-//                    // Handle the empty entry for each line
-//                    String[] dataArray = parseByRegex(line,regex,originalFields.length);
-//                    for (Map.Entry<Integer, SubAttConvertor> entry : ProcessingContext.merge.entrySet()) {
-//                        int key = entry.getKey();
-//                        SubAttConvertor curConv = entry.getValue();
-//                        String[] subArr = curConv.parseByRegex(dataArray[key]);
-//                        dataArray = mergeArr(dataArray, key, subArr);
-//                    }
-//                    writer.write(dataArray);
-//                }
-//
-//                reader.close();
-//                writer.close();
-//
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            System.out.println(e.getMessage());
-//        } catch (InvalidLoadOptionException e) {
-//            e.printStackTrace();
-//            System.out.println(e.getMessage());
-//        }
-//
-//    }
+    public static void CSVParseAndWrite(String inputPath, String outputDir, Field[] originalFields, String regex){
+        Field[] fields = originalFields;
+        System.out.println("Input file schema: ");
+        for(Field f:fields){
+            System.out.print("\t" + f.getFieldName() +" " +f.getDataType().toString());
+        }
+        System.out.print("\n");
+        if (!ProcessingContext.context.isEmpty()){
+            System.out.println("Sub-column schema transformation: ");
+            for (int i=fields.length-1; i>=0; i--){
+                if (ProcessingContext.context.containsKey(fields[i].getFieldName())){
+                    String[] curMeta = ProcessingContext.context.get(fields[i].getFieldName());
+                    SubAttConvertor curConv = new SubAttConvertor(fields[i].getFieldName(), curMeta[0],curMeta[2],curMeta[1], fields[i].getDataType(),(curMeta.length==4?curMeta[3]:null));
+                    ProcessingContext.merge.put(i,curConv);
+                    Field[] subFields = new Field[curConv.getNumSubCols()];
+
+                    for(int j=0;j<curConv.getNumSubCols();j++){
+                        subFields[j] = new Field(curConv.getSubcol()[j],curConv.getSubTypes()[j]);
+                    }
+                    fields = mergeArr(fields,i,subFields);
+                }
+            }
+            System.out.println("Translated file schema: ");
+            for(Field f:fields){
+                System.out.print("\t" + f.getFieldName() +" " +f.getDataType().toString());
+            }
+
+        }
+
+        try {
+            FileUtils.deleteDirectory(new File(outputDir));
+            BufferedReader reader = new BufferedReader(new FileReader(new File(inputPath)));
+            Map<String, String> map = new HashMap<String, String>();
+            map.put("complex_delimiter_level_1", "#");
+            CarbonWriter writer = CarbonWriter.builder()
+                    .outputPath(outputDir)
+                    .withLoadOptions(map)
+                    .buildWriterForCSVInput(new Schema(fields));
+            String line;
+            boolean skipHeader = true;
+            // Skip header line
+            line = skipHeader ? reader.readLine() : null;
+
+            if (ProcessingContext.merge.isEmpty()){
+                while ((line = reader.readLine()) != null) {
+                    // Handle the empty entry for each line
+                    String[] dataArray = parseByRegex(line,regex,fields.length);
+                    writer.write(dataArray);
+                }
+
+                reader.close();
+                writer.close();
+            }
+            else{
+                while ((line = reader.readLine()) != null) {
+                    // Handle the empty entry for each line
+                    String[] dataArray = parseByRegex(line,regex,originalFields.length);
+                    for (Map.Entry<Integer, SubAttConvertor> entry : ProcessingContext.merge.entrySet()) {
+                        int key = entry.getKey();
+                        SubAttConvertor curConv = entry.getValue();
+                        String[] subArr = curConv.parseByRegex(dataArray[key]);
+                        dataArray = mergeArr(dataArray, key, subArr);
+                    }
+                    writer.write(dataArray);
+                }
+
+                reader.close();
+                writer.close();
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        } catch (InvalidLoadOptionException e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+
+    }
 
 
 }
